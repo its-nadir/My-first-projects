ec83e95d63f2bddcc8f673384fa8855c81477c56
Abdelfateh Nadir Djelti <168342468+its-nadir@users.noreply.github.com>
2025-02-03 13:55:10 +0100

    Update TREASURE_MAP.py

--- My Fisrt small projects in Python/TREASURE_MAP.py
+++ My Fisrt small projects in Python/TREASURE_MAP.py
@@ -5,7 +5,7 @@ map = [line1, line2, line3]
 print("Hiding your treasure! X marks the spot.")
 position = input("Where do you want to put the treasure?") 
 indices = {'A': 0, 'B': 1, 'C': 2}
-y=indices[position[0].upper()] 
+y=indices[position[0].upper()]
 x=int(position[1])-1
 map[x][y]="X"
 print(f"{line1}\n{line2}\n{line3}")
