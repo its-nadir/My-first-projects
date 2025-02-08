124cc2c80d98e0d936e75a5399db4c86c34a84d4
Abdelfateh Nadir Djelti <168342468+its-nadir@users.noreply.github.com>
2025-02-08 14:12:00 +0100

    Create Love_score.py

---
+++ My Fisrt small projects in Python/Love_score.py
@@ -0,0 +1,15 @@
+def calculate_love_score(name1, name2):
+    score1 = 0
+    score2 = 0
+    for letter in name1 + name2:
+        if letter in ("TRUE"):
+            score1 += 1
+    for letter in name1 + name2:
+        if letter in ("LOVE"):
+            score2 += 1
+    print(str(score1) + str(score2))
+
+name1 = input("Enter the first name: ").upper()
+name2 = input("Enter the second name: ").upper()
+
+calculate_love_score(name1, name2)
